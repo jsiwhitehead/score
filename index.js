aba6ba6e9e6396e81ddd03597545c1a277aeca99
Jon Whitehead <jsiwhitehead@gmail.com>
2020-11-08 01:12:24 +0000

    Added smooth key change

--- index.js
+++ index.js
@@ -3,24 +3,22 @@ const moddist = (a, b, n) => Math.min(mod(a - b, n), mod(b - a, n));
 
 const draw = SVG().addTo('body').size('100%', '100%').css({ display: 'block' });
 
-// const colors = ['#eee', '#ccc', '#aaa', '#888'];
-const colors = ['#e1f5fe', '#b3e5fc', '#4fc3f7', '#0288d1'];
-// const colors = ['yellow', 'orange', 'red', 'darkred'];
+const getColor = (s) => chroma.hsl(210, 1, 1 - s * 0.1).hex();
 
-const width = 6;
+const width = 6 * 8;
 
-const drawBar = (num, key, notes) => {
+const drawBar = (num, key, note) => {
   const x = num % width;
   const y = Math.floor(num / width);
-  const baseX = 40 + x * 160;
+  const baseX = 40 + x * 20;
   const baseY = 20 + y * 200;
   for (let i = 0; i < 4; i++) {
     for (const [k, s] of key) {
       const pos = 28 - (i * 7 + k);
       draw
-        .rect(160, 5)
+        .rect(20, 5)
         .move(baseX, baseY + pos * 5)
-        .fill(colors[s - 1]);
+        .fill(getColor(s));
     }
   }
   if (x === width - 1) {
@@ -30,25 +28,27 @@ const drawBar = (num, key, notes) => {
         draw
           .rect(20, 5)
           .move(20, baseY + 200 + pos * 5)
-          .fill(colors[s - 1]);
+          .fill(getColor(s));
       }
     }
   }
-  draw
-    .rect(1, 140)
-    .move(baseX, baseY + 5)
-    .fill(colors[3]);
-  for (const [i, n] of notes.entries()) {
-    const pos = 28 - n;
-    if (mod(n, 1) === 0.5) {
+  if (num % 8 === 0) {
+    draw
+      .rect(1, 140)
+      .move(baseX, baseY + 5)
+      .fill(getColor(4));
+  }
+  if (note !== null) {
+    const pos = 28 - note;
+    if (mod(note, 1) === 0.5) {
       draw
         .rect(9, 5)
-        .move(baseX + 5.5 + i * 20, baseY + 2.5 + pos * 5)
+        .move(baseX + 5.5, baseY + 2.5 + pos * 5)
         .fill('black');
     } else {
       draw
         .circle(9)
-        .move(baseX + 5.5 + i * 20, baseY + 0.5 + pos * 5)
+        .move(baseX + 5.5, baseY + 0.5 + pos * 5)
         .fill('black');
     }
   }
@@ -70,53 +70,66 @@ const getRotationKey = (fifths) => {
     );
   });
   const index = rotations.findIndex((r) => r === Math.min(...rotations));
-  if (rotations[index] < 2) return -index - 3;
-  if (rotations[index] < 8) return -index - 2;
-  if (rotations[index] < 32) return -index - 1;
-  if (rotations[index] < 128) return -index;
-  return false;
+  if (rotations[index] < 32) return [-index, 4];
+  if (rotations[index] < 64) return [-index, 5];
+  if (rotations[index] < 128) return [-index, 6];
+  return null;
 };
 const getKeyBase = (fifths) => {
   const full = getRotationKey(fifths);
-  if (full !== false) return full;
+  if (full) return full;
   for (const f of fifths) {
     const partial = getRotationKey(fifths.filter((x) => x !== f));
-    if (partial !== false) return partial;
+    if (partial) return partial;
   }
-  return (
+  return [
     Math.round(
       Math.atan2(
         fifths.reduce((res, s) => res + Math.sin(s * pi6), 0),
         fifths.reduce((res, s) => res + Math.cos(s * pi6), 0)
       ) / pi6
-    ) - 3
-  );
+    ) - 3,
+    6,
+  ];
 };
 
-const getKeyAndNotes = (notes) => {
+const getKey = (notes) => {
   const fifths = Object.keys(
     notes
-      .filter((n) => typeof n === 'number')
+      .reduce(
+        (res, n) =>
+          typeof n === 'number'
+            ? [...res, n]
+            : [
+                ...res,
+                parseInt(n, 10) - 1,
+                parseInt(n, 10) + 1,
+                parseInt(n, 10) + 6,
+              ],
+        []
+      )
       .reduce((res, n) => ({ ...res, [mod(n * 7, 12)]: true }), {})
   ).map((k) => parseInt(k, 10));
-  const base = mod(getKeyBase(fifths) * 7, 12) - 12;
-  const key = { 0: true, 2: true, 3: true, 6: true };
-  const scaleNotes = notes
-    .map((n) => parseInt(n, 10))
-    .map((n) => {
-      const aboveBase = n - base;
-      const octave = Math.floor(aboveBase / 12);
-      const note = mod(aboveBase, 12);
-      const scaleNote = note * 0.5 + (note > 6 ? 0.5 : 0);
-      if (scaleNote === 0) delete key[0];
-      if (scaleNote === 3) delete key[2];
-      return octave * 7 + scaleNote;
-    });
-  return {
-    notes: scaleNotes,
-    key: Object.keys(key).map((k) => parseInt(k)),
-    base,
-  };
+  const [keyBase, size] = getKeyBase(fifths);
+  const base = mod(keyBase * 7, 12) - 12;
+  const key = [
+    [2, 3, 5, 6],
+    [2, 3, 6],
+    [3, 6],
+  ][size - 4];
+  return { key, base, size };
+};
+
+const getKeyNote = (base, size, note) => {
+  const aboveBase = parseInt(note, 10) - base;
+  const octave = Math.floor(aboveBase / 12);
+  const n = mod(aboveBase, 12);
+  const scaleNote = [
+    [0, 0.5, 1, 1.5, 2, 2.5, 3.5, 4, 4.5, 5, 5.5, 6.5],
+    [0, 0.5, 1, 1.5, 2, 2.5, 3.5, 4, 4.5, 5, 5.5, 6],
+    [0, 0.5, 1, 1.5, 2, 2.5, 3, 4, 4.5, 5, 5.5, 6],
+  ][size - 4][n];
+  return octave * 7 + scaleNote;
 };
 
 const updateKey = (base, key) => {
@@ -125,14 +138,19 @@ const updateKey = (base, key) => {
   for (const k of key) {
     if (!result.some((x) => x[0] === k)) {
       for (const x of result) {
-        if (moddist(k, x[0], 7) === 1) x[1] = Math.max(x[1] - 2, 0);
+        if (moddist(k, x[0], 7) === 1) x[1] = Math.max(x[1] - 0, 0);
       }
       result = [...result, [k, 0]];
     }
   }
+  if (result.filter((k) => k[1] >= 1).length < 2) {
+    for (const k of result) {
+      k[1] = Math.max(k[1], 1);
+    }
+  }
   for (const k of result) {
     k[1] = Math.min(
-      k[1] + (result.some((x) => moddist(x[0], k[0], 7) === 1) ? 1 : 2),
+      k[1] + (result.some((x) => moddist(x[0], k[0], 7) === 1) ? 0.05 : 0.05),
       4
     );
   }
@@ -162,13 +180,13 @@ const lines = [
   [22, 21, 19, 29, 28, 26, 24, 22],
   [21, 19, 17, 29, 24, 29, 21, 24],
   [17, 19, 21, 24, 22, 21, 19, 17],
-  ['23', 17, 20, 19, 20, 17, '23', 17, 26],
+  ['23', 17, 20, 19, 20, 17, '23', 17],
   [26, 17, 20, 19, 20, 17, 26, 17],
   [15, 19, 24, 26, 27, 24, 19, 17],
   [15, 19, 24, 26, 27, 24, 21, 19],
 
-  ['18', 21, '18', 21, 24, 21, 24, 21, 27],
-  ['18', 21, '18', 21, 24, 21, 24, 21, 27],
+  ['18', 21, '18', 21, 24, 21, 24, 21],
+  ['18', 21, '18', 21, 24, 21, 24, 21],
   [22, 21, 19, 22, 21, 22, 24, 21],
   [22, 21, 19, 17, 15, 14, 12, 10],
   [9, 15, 17, 15, 17, 15, 17, 15],
@@ -187,8 +205,8 @@ const lines = [
 
   [5, 12, 17, 19, 21, 17, 19, 21],
   [5, 12, 17, 19, 21, 17, 19, 21],
-  [5, 12, 16, 19, 22, 28, 29],
-  [12, 14, 15, 17, 19, 21, 22],
+  [5, 12, 16, 19, 22, 28, 29, null],
+  [12, 14, 15, 17, 19, 21, 22, null],
   [24, 21, 17, 19, 21, 22, 24, 26],
   [27, 24, 21, 22, 24, 26, 27, 29],
   ['30', 29, 28, 29, 29, 27, 26, 27],
@@ -197,13 +215,13 @@ const lines = [
   [5, 12, 17, 21, 24, 26, 27, 24],
   [26, 22, 17, 15, 14, 10, 12, 14],
   [5, 12, 16, 19, 22, 24, '25', 22],
-  [28, 26, 24, '25', '25', 24, '23', 24, 22],
+  [28, 26, 24, '25', '25', 24, '23', 24],
   [24, 22, 21, 22, 22, 19, 16, 14],
   [12, 16, 19, 22, 24, 28, 29, 28],
   [29, 24, 21, 19, 21, 24, 17, 21],
   [12, 17, 16, 14, 12, 10, 9, 7],
 
-  [5, 27, 26, 24, 22, 21, 19],
+  [5, 27, 26, 24, 22, 21, 19, null],
   [17, 27, 26, 24, 22, 21, 19, 17],
   [16, 26, 24, 22, 21, 19, 17, 16],
   [14, 24, 22, 21, 19, 17, 16, 14],
@@ -238,20 +256,29 @@ const lines = [
 let prevBase;
 let baseKey;
 let prevOffset = 0;
+let currentNotes = [];
 for (const [i, l] of lines.entries()) {
-  const { key, notes, base } = getKeyAndNotes(l);
-  const diff = prevBase === undefined ? 0 : base - prevBase;
-  const offset =
-    prevOffset + (diff < 0 ? -1 : 1) * Math.ceil(Math.abs(diff / 2));
-  baseKey = updateKey(
-    baseKey,
-    key.map((k) => mod(k + offset, 7))
-  );
-  drawBar(
-    i,
-    baseKey,
-    notes.slice(0, 8).map((n) => n + offset)
-  );
-  prevBase = base;
-  prevOffset = offset;
+  for (const [j, n] of l.slice(0, 8).entries()) {
+    if (n !== null) currentNotes.unshift(n);
+    currentNotes = currentNotes.slice(0, 12);
+
+    const { key, base, size } = getKey(currentNotes);
+    const diff = prevBase === undefined ? 0 : base - prevBase;
+    const offset =
+      prevOffset + (diff < 0 ? -1 : 1) * Math.ceil(Math.abs(diff / 2));
+    baseKey = updateKey(
+      baseKey,
+      key.map((k) => mod(k + offset, 7))
+    );
+
+    if (n !== null) {
+      const keyNote = getKeyNote(base, size, n);
+      drawBar(i * 8 + j, baseKey, keyNote + offset);
+    } else {
+      drawBar(i * 8 + j, baseKey, null);
+    }
+
+    prevBase = base;
+    prevOffset = offset;
+  }
 }
